68ed0c95586d6b9afe40b7ebd77226270753c476
arb1ona <arbiona.shahu@gmail.com>
2020-12-15 22:42:50 +0100

    fetch countries and display as dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { MenuItem, FormControl, Select } from "@material-ui/core";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    //async->send a request
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+
+  //https://disease.sh/v3/covid_19/countries
   return (
     <div className="app">
-     <h1>Arbiona's is working on!</h1>
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {/* Loop through alll the countries
+            and show a drop down list of option */}
+
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+
+      {/* Header */}
+      {/* Title+Select input dropdown field */}
+
+      {/* InfoBox*/}
+      {/* InfoBox*/}
+      {/* InfoBox*/}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   );
 }
