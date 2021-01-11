c4c004fea48c1ece126c3cf595fa02c226c09e1a
arb1ona <arbiona.shahu@gmail.com>
2021-01-11 09:36:35 +0100

    Click + Active + Change of Colour

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
   const [mapZoom, setMapZoom] = useState(2);
   const [mapCenter, setMapCenter] = useState({ lat: 34, lng: -40 });
   const [mapCountries, setMapCountries] = useState([]);
+  const [casesType, setCasesType] = useState("cases");
 
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/all")
@@ -93,29 +94,42 @@ function App() {
 
         <div className="app__stats">
           <InfoBox
+            isRed
+            active={casesType === "cases"}
+            onClick={(e) => setCasesType("cases")}
             title="Coronavirus cases"
             cases={prettyPrintStat(countryInfo.todayCases)}
             total={prettyPrintStat(countryInfo.cases)}
           />
           <InfoBox
+            active={casesType === "recovered"}
+            onClick={(e) => setCasesType("recovered")}
             title="Recovered"
             cases={prettyPrintStat(countryInfo.todayRecovered)}
             total={prettyPrintStat(countryInfo.recovered)}
           />
           <InfoBox
+            isRed
+            active={casesType === "deaths"}
+            onClick={(e) => setCasesType("deaths")}
             title="Deaths"
             cases={prettyPrintStat(countryInfo.todayDeaths)}
             total={prettyPrintStat(countryInfo.deaths)}
           />
         </div>
-        <Map countries={mapCountries} center={mapCenter} zoom={mapZoom} />
+        <Map
+          casesType={casesType}
+          countries={mapCountries}
+          center={mapCenter}
+          zoom={mapZoom}
+        />
       </div>
       <Card className="app__right">
         <CardContent>
-          <h3>live cases by country</h3>
+          <h3>Live cases by country</h3>
           <Table countries={tableData} />
-          <h3>Worldwide new cases</h3>
-          <LineGraph />
+          <h3 className="app__graphTitle">Worldwide new {casesType}</h3>
+          <LineGraph className="app__graph" casesType={casesType} />
         </CardContent>
       </Card>
     </div>
--- src/InfoBox.js
+++ src/InfoBox.js
@@ -2,9 +2,17 @@ import React from "react";
 import "./InfoBox.css";
 import { Card, CardContent, Typography } from "@material-ui/core";
 
-function InfoBox({ title, cases, total }) {
+function InfoBox({ title, cases, isRed, active, total, ...props }) {
   return (
-    <Card className="infoBox">
+    <Card
+      onClick={props.onClick}
+      // className={`infoBox ${active && "infoBox--selected"} ${
+      //   isRed && "infoBox--red"
+      // }`}
+      className={`infoBox ${active && "infoBox--selected"} ${
+        isRed && "infoBox--red"
+      }`}
+    >
       <CardContent>
         {/*title  */}
         <Typography className="infoBox__title" color="textSecondary">
@@ -12,7 +20,9 @@ function InfoBox({ title, cases, total }) {
         </Typography>
 
         {/* numb of cases */}
-        <h2 className="infoBox__cases">{cases}</h2>
+        <h2 className={`infoBox__cases ${!isRed && "infoBox__cases--green"}`}>
+          {cases}
+        </h2>
 
         {/* total */}
         <Typography className="infoBox__total" color="textSecondary">
