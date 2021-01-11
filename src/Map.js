c7bf834b178e4e3be09211b807cd65aed7b1432e
arb1ona <arbiona.shahu@gmail.com>
2021-01-11 06:11:21 +0100

    Data/Circles On Map

--- src/Map.js
+++ src/Map.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "./Map.css";
 import { MapContainer as LeafletMap, TileLayer } from "react-leaflet";
+import { showDataOnMap } from "./util";
 
-function Map({ countries, center, zoom }) {
+function Map({ countries, casesType, center, zoom }) {
   return (
     <div className="map">
       <LeafletMap center={center} zoom={zoom}>
@@ -11,6 +12,7 @@ function Map({ countries, center, zoom }) {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
         {/* Loop throuugh and draw circles on the screen-bigger or smaller */}
+        {showDataOnMap(countries, casesType)}
       </LeafletMap>
     </div>
   );
